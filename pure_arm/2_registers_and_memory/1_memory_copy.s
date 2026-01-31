befe2e0d5c74bf9f20c0e77b6d91799424672571
Nataniel Dziadzia <sumplous.sumplous@gmail.com>
2026-01-31 18:44:44 +0100

    memory copy, small program of repeated steps that should be done in a loop

--- pure_arm/2_registers_and_memory/1_memory_copy.s
+++ pure_arm/2_registers_and_memory/1_memory_copy.s
@@ -1,7 +1,42 @@
 .global _start 
 
 _start:
+  ldr r0, =instr
+  ldr r1, =outstr
+  ldrb r3, [r0] @S
+  strb r3, [r1]
+  add r0, #1
+  add r1, #1
+  ldrb r3, [r0]
+  strb r3, [r1]
+  add r0, #1
+  add r1, #1
+  ldrb r3, [r0]
+  strb r3, [r1]
+  add r0, #1
+  add r1, #1
+  ldrb r3, [r0]
+  strb r3, [r1]
+  add r0, #1
+  add r1, #1
+  ldrb r3, [r0]
+  strb r3, [r1]
+  add r0, #1
+  add r1, #1
+  ldrb r3, [r0]
+  strb r3, [r1]
 
+  mov r3, #'\n'
+  strb r3, [r1]
+
+  mov r7, #4
+  mov r0, #1
+  ldr r1, =outstr
+  mov r2, #7
+  svc 0
+  mov r7, #1
+  mov r0, #0
+  svc 0
 
 .data
 instr:      .ascii      "SUMMER"
